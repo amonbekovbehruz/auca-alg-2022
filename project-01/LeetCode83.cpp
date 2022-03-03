1e8e270ea4af2e365056ade1ab8ec7c0e97f01c5
Behruz Amonbekov <amonbekov_b@auca.kg>
2022-03-03 10:19:04 +0100

    Finsihed LeetCode83 in Project 1

---
+++ project-01/LeetCode83.cpp
@@ -0,0 +1,36 @@
+struct ListNode {
+          int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+       if (head != nullptr)
+       {
+           ListNode* prev = head;
+           ListNode* curr = prev->next;
+           
+           while(curr != nullptr)
+           {
+               if(prev->val == curr->val)
+               {
+                prev->next = curr->next;
+                delete curr;
+                curr = prev->next;
+               } else {
+                   prev = curr;
+                   curr = curr->next;
+               }
+               
+           }
+           
+       }
+        return head;
+        
+    }
+};
